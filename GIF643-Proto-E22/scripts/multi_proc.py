4aa4d9fd298351ec837a8849e5d1914101646e36
Paul du Réau <pauldrdlg@gmail.com>
2022-05-10 13:44:26 -0400

    Validation asset_conv ready

--- GIF643-Proto-E22/scripts/multi_proc.py
+++ GIF643-Proto-E22/scripts/multi_proc.py
@@ -9,7 +9,7 @@ NUM_PROCESSES = 4
 NUM_THREADS = 4
 
 def run_process(task_desc):
-    p = subprocess.Popen(["./asset_conv", str(NUM_THREADS)], stdin=subprocess.PIPE)
+    p = subprocess.Popen(["../build/asset_conv", str(NUM_THREADS)], stdin=subprocess.PIPE)
     p.communicate(input=task_desc.encode())
 
 
